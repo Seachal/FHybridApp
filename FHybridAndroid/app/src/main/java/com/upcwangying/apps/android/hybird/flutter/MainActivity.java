e8ba6a8f103c776da35e87647663aa71bb814180
upcwangying <upcwangying@126.com>
2020-08-10 18:49:23 +0800

    initial commit

---
+++ FHybridAndroid/app/src/main/java/com/upcwangying/apps/android/hybird/flutter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.upcwangying.apps.android.hybird.flutter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
